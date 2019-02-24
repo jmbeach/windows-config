5d2635c1ec353c409b2d050cdec99e85be8e9386
Jared Beach <jaredbeachdesign@gmail.com>
2019-02-24 14:00:39 -0600

    updates deploy and get scripts

--- deploy.ps1
+++ deploy.ps1
@@ -1,2 +1,4 @@
 Copy-Item .\user-profile.ps1 $env:CMDER_ROOT\config\ -Force
 Copy-Item .\prompt-char.txt "$env:CMDER_ROOT\config\prompt-char.txt"
+Copy-Item -re .\custom-scripts $home
+Copy-Item .\user-ConEmu.xml "$env:CMDER_ROOT\"
