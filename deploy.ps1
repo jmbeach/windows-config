1372aae0b026293ebea022ecccd1c9975683ab99
Jared Beach <jaredbeachdesign@gmail.com>
2019-02-24 14:24:13 -0600

    more import and export of visual studio settings

--- deploy.ps1
+++ deploy.ps1
@@ -3,3 +3,11 @@ Copy-Item .\prompt-char.txt "$env:CMDER_ROOT\config\prompt-char.txt"
 Copy-Item -re .\custom-scripts $home
 Copy-Item .\user-ConEmu.xml "$env:CMDER_ROOT\"
 Copy-Item .\visual-studio\code-snippets\* "$env:USERPROFILE\Documents\Visual Studio 2017\Code Snippets\"
+
+$cwd = (Get-Item .).FullName
+
+# export visual studio settings and copy here https://stackoverflow.com/a/48663477/1834329
+$outFileName = "$cwd\visual-studio\vs17-settings.xml"
+$filenameEscaped="`"$outFileName`""
+$dte = [System.Runtime.InteropServices.Marshal]::GetActiveObject("VisualStudio.DTE.15.0") 
+$dte.ExecuteCommand("Tools.ImportandExportSettings", '/import:' + $filenameEscaped)
--- get-latest.ps1
+++ get-latest.ps1
@@ -3,3 +3,11 @@ Copy-Item -re -fo $home\custom-scripts .\
 Copy-Item "$env:CMDER_ROOT\config\prompt-char.txt" .
 Copy-Item "$env:CMDER_ROOT\config\user-ConEmu.xml" .
 Copy-Item -re -fo "$env:USERPROFILE\Documents\Visual Studio 2017\Code Snippets\*" .\visual-studio\code-snippets
+
+$cwd = (Get-Item .).FullName
+
+# export visual studio settings and copy here https://stackoverflow.com/a/48663477/1834329
+$outFileName = "$cwd\visual-studio\vs17-settings.xml"
+$filenameEscaped="`"$outFileName`""
+$dte = [System.Runtime.InteropServices.Marshal]::GetActiveObject("VisualStudio.DTE.15.0") 
+$dte.ExecuteCommand("Tools.ImportandExportSettings", '/export:'+$filenameEscaped)
