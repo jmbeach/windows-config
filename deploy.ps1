0f680fcb87e3383d360cfb64a54577825e61dc2c
Jared Beach <jaredbeachdesign@gmail.com>
2019-02-24 14:34:46 -0600

    use re and fe flags

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,8 @@
 Copy-Item .\user-profile.ps1 $env:CMDER_ROOT\config\ -Force
 Copy-Item .\prompt-char.txt "$env:CMDER_ROOT\config\prompt-char.txt"
-Copy-Item -re .\custom-scripts $home
+Copy-Item -re -fo .\custom-scripts $home
 Copy-Item .\user-ConEmu.xml "$env:CMDER_ROOT\"
-Copy-Item .\visual-studio\code-snippets\* "$env:USERPROFILE\Documents\Visual Studio 2017\Code Snippets\"
+Copy-Item -re -fo .\visual-studio\code-snippets\* "$env:USERPROFILE\Documents\Visual Studio 2017\Code Snippets\"
 
 $cwd = (Get-Item .).FullName
 
