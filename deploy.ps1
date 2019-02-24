24ae46c77ea1edebd312b27e1e79c924a17b12f9
Jared Beach <jaredbeachdesign@gmail.com>
2019-02-24 14:08:51 -0600

    updates get for visual studio

--- deploy.ps1
+++ deploy.ps1
@@ -2,3 +2,4 @@ Copy-Item .\user-profile.ps1 $env:CMDER_ROOT\config\ -Force
 Copy-Item .\prompt-char.txt "$env:CMDER_ROOT\config\prompt-char.txt"
 Copy-Item -re .\custom-scripts $home
 Copy-Item .\user-ConEmu.xml "$env:CMDER_ROOT\"
+Copy-Item .\visual-studio\code-snippets\* "$env:USERPROFILE\Documents\Visual Studio 2017\Code Snippets\"
--- get-latest.ps1
+++ get-latest.ps1
@@ -2,3 +2,4 @@ Copy-Item "$env:CMDER_ROOT/config/user-profile.ps1" .
 Copy-Item -re -fo $home\custom-scripts .\
 Copy-Item "$env:CMDER_ROOT\config\prompt-char.txt" .
 Copy-Item "$env:CMDER_ROOT\config\user-ConEmu.xml" .
+Copy-Item -re -fo "$env:USERPROFILE\Documents\Visual Studio 2017\Code Snippets\*" .\visual-studio\code-snippets
