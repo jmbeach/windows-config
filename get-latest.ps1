c96291b165e561335c403a9e1e68457c8cb40e58
Jared Beach <jaredbeachdesign@gmail.com>
2019-10-19 10:36:40 -0500

    add default path to conemu for get latest

--- get-latest.ps1
+++ get-latest.ps1
@@ -1,3 +1,7 @@
+if ([string]::IsNullOrEmpty($env:CMDER_ROOT)) {
+    $env:CMDER_ROOT = 'C:\tools\Cmder'
+}
+
 Copy-Item "$env:CMDER_ROOT/config/user-profile.ps1" .
 Copy-Item -re -fo $home\custom-scripts\* .\custom-scripts
 Copy-Item "$env:CMDER_ROOT\config\prompt-char.txt" .
