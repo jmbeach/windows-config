1382a4ba94fcd89a4bac5cdedf37d3cb4cb90b53
Jared Beach <jaredbeachdesign@gmail.com>
2019-12-22 16:09:31 -0600

    adds very cool table writing utility

--- custom-scripts/utilities.ps1
+++ custom-scripts/utilities.ps1
@@ -11,12 +11,15 @@ function Search-C-FileName([string] $fileName, [string] $fileType) {
 	}
 }
 
-function go-yfai () {
-	cd D:\Code\Customers\yfai\
-}
-
 function Convert-Pdfs2Txt () {
 	Get-ChildItem *.pdf | foreach { python C:\Python27\Scripts\pdf2txt.py $_.Name | Out-File -Encoding ascii $($_.Name + '.txt') }
+}
+
+function Convert-ObjectToHash ($asObj) {
+	$result = @{};
+	$asObj.PSObject.Properties | ForEach-Object {
+        $result[$_.Name] = $_.Value;
+    }
 }
 
 function Convert-Htmls2Txt () {
@@ -81,12 +84,6 @@ function Get-RunningProcessCount () {
 	Write-Host $i
 }
 
-function tasklist-v () {
-	Get-Process | foreach {
-		$_ | Format-Table Name, Id, CPU, PrivateMemorySize, Path
-	}
-}
-
 function Kill-Unessential () {
 	$killable = Get-JsonFromFile('~/custom-scripts/unessential.json')
 	$killable.processes | foreach {
@@ -120,5 +117,101 @@ function Get-Wallpapers() {
 	dir $location | foreach {
 		if ($_.Length -lt 100kb) { return }
 		cp $_.FullName $("$home\Pictures\Wallpapers\" + $_.Name + ".jpg");
+	}
+}
+
+function Write-Tabular([array]$list, [scriptblock]$highlightExpression, $headerUnderlineColor, $highlightColor) {
+	$first = $list[0];
+	$members = $first | Get-Member | Where-Object { $_.MemberType -ne 'Method' };
+	if ($null -eq $headerUnderlineColor) {
+		$headerUnderlineColor = 'Blue';
+	}
+
+	if ($null -eq $highlightColor) {
+		$highlightColor = 'Blue';
+	}
+
+	$writingDetails = @{};
+	$members | ForEach-Object {
+		$writingDetails[$_.Name] = @{
+			maxLength = $_.Name.Length;
+		}
+	}
+
+	$list | ForEach-Object {
+		$item = $_;
+		$members | ForEach-Object {
+			$member = $_;
+			$details = $writingDetails[$member.Name];
+			if ($item.PSObject.Properties[$member.Name].Value.ToString().Length -gt $details.maxLength) {
+				$writingDetails[$member.Name].maxLength = $item.PSObject.Properties[$member.Name].Value.ToString().Length;
+			}
+		}
+	}
+
+	$header = '';
+	$underline = '';
+	for ($i = 0; $i -lt $members.Length; $i++) {
+		$member = $members[$i];
+		$details = $writingDetails[$member.Name];
+		$tabCount = ($details.maxLength - $member.Name.Length) / 9;
+		$header += $member.Name;
+		$(1..$member.Name.Length) | ForEach-Object { $underline += "=" };
+		if ($tabCount -gt 0 -and $i -ne $members.Length - 1) {
+			$(1..$tabCount) | ForEach-Object { $header += "`t" };
+			$(1..$tabCount) | ForEach-Object { $underline += "========" }
+		}
+
+		if ($i -ne $members.Length - 1) {
+			$header += "`t| ";
+			$underline += "`t| ";
+		}
+	}
+
+	Write-Host $header;
+	$underlines = $underline.Split("|");
+	for ($i = 0; $i -lt $underlines.Length; $i++) {
+		$underline = $underlines[$i];
+		Write-Host -NoNewline -ForegroundColor $headerUnderlineColor $underline
+		if ($i -ne $underlines.Length - 1) {
+			Write-Host "|" -NoNewline
+		}
+	}
+
+	# Adds a new line after the underline section of header
+	Write-Host
+
+	$list | ForEach-Object {
+		$item = $_;
+		$line = '';
+		for ($i = 0; $i -lt $members.Length; $i++) {
+			$member = $members[$i];
+			$val = $item.PSObject.Properties[$member.Name].Value.ToString();
+			$details = $writingDetails[$member.Name];
+			$tabCount = ($details.maxLength - $val.Length) / 9;
+			$line += $val
+			if ($tabCount -gt 0 -and $i -ne $members.Length - 1) {
+				$(1..$tabCount) | ForEach-Object { $line += "`t" };
+			}
+
+			if ($i -ne $members.Length - 1) {
+				$line += "`t| "
+			}
+		}
+
+		if ($null -ne $highlightExpression -and $($item | &$highlightExpression)) {
+			$lineParts = $line.Split('|');
+			for ($j = 0; $j -lt $lineParts.Length; $j++) {
+				$part = $lineParts[$j];
+				Write-Host -ForegroundColor $highlightColor $part -NoNewline;
+				if ($j -ne $lineParts.Length - 1) {
+					Write-Host "|" -NoNewline;
+				}
+			}
+
+			Write-Host;
+		} else {
+			Write-Host $line;
+		}
 	}
 }
