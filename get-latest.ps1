0af88fe87f425c8f1ff620fe012062e23676a1ad
jmbeach <beachj@ironsolutions.com>
2020-01-31 08:05:16 -0600

    deploy custom scripts separately

--- get-latest.ps1
+++ get-latest.ps1
@@ -3,7 +3,6 @@ if ([string]::IsNullOrEmpty($env:CMDER_ROOT)) {
 }
 
 Copy-Item "$env:CMDER_ROOT/config/user_profile.ps1" .
-Copy-Item -re -fo $home\custom-scripts\* .\custom-scripts
 Copy-Item "$env:CMDER_ROOT\config\prompt-char.txt" .
 Copy-Item "$env:CMDER_ROOT\config\user-ConEmu.xml" .
 Copy-Item -re -fo "$env:USERPROFILE\Documents\Visual Studio 2017\Code Snippets\*" .\visual-studio\code-snippets
