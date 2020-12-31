065cdf80bb943fba13f3365172d657505a7be68d
jmbeach <jaredbeachdesign@gmail.com>
2020-12-31 03:49:14 -0600

    fix naming mistake

--- deploy.ps1
+++ deploy.ps1
@@ -7,19 +7,15 @@ if ($IsLinux) {
     $codeSettingsPath = "$HOME/.config/Code/User"
 }
 
-Copy-Item .\user_profile.ps1 $env:CMDER_ROOT\config\ -Force
-Copy-Item .\prompt-char.txt "$env:CMDER_ROOT\config\prompt-char.txt"
-Copy-Item .\user-ConEmu.xml "$env:CMDER_ROOT\"
-Copy-Item -re -fo .\visual-studio\code-snippets\* "$env:USERPROFILE\Documents\Visual Studio 2015\Code Snippets\";
+
+if ($IsWindows) {
+    Copy-Item .\user_profile.ps1 $env:CMDER_ROOT\config\ -Force
+    Copy-Item .\prompt-char.txt "$env:CMDER_ROOT\config\prompt-char.txt"
+    Copy-Item .\user-ConEmu.xml "$env:CMDER_ROOT\"
+    Copy-Item -re -fo .\visual-studio\code-snippets\* "$env:USERPROFILE\Documents\Visual Studio 2015\Code Snippets\";
+    Copy-Item .\shrug.txt "$HOME\" -Force
+    Copy-Item icons $HOME
+}
+
 Copy-Item -Force .\vscode-config\keybindings.json $codeSettingsPath
-Copy-Item -Force .\vscode-snippets\ $codeSettingsPath;
-Copy-Item .\shrug.txt "$HOME\" -Force
-Copy-Item icons $HOME
-
-$cwd = (Get-Item .).FullName
-
-# export visual studio settings and copy here https://stackoverflow.com/a/48663477/1834329
-# $outFileName = "$cwd\visual-studio\vs17-settings.xml"
-# $filenameEscaped="`"$outFileName`""
-# $dte = [System.Runtime.InteropServices.Marshal]::GetActiveObject("VisualStudio.DTE.15.0") 
-# $dte.ExecuteCommand("Tools.ImportandExportSettings", '/import:' + $filenameEscaped)
+Copy-Item -Force -Recurse .\vscode-snippets\* $codeSettingsPath;
